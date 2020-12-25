3f445623a31bebe5d6711ec3a9bb58da0508efd0
Daniel Ben-Shloosh <danielbenshloosh@gmail.com>
2020-12-25 03:03:44 +0200

    Adding files

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:91a49254-2bef-482d-895f-09348fc4bf15",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_NYGeGwMDr",
+                        "AppClientId": "5bp08eumvkvnjliit8boqqt6p6",
+                        "AppClientSecret": "1scktaein1d4ci5uma1dvg6t0h20n0ts7dmqbullqrgr1bp8ehnp",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
